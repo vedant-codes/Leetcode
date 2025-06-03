932145f89cf50e9070bf987c2dae302e0f809e3a
vedant-codes <vedantshinde.vs12@gmail.com>
2025-06-03 15:23:16 +0530

    Create 169-Majority_array.c

---
+++ 169-Majority_array.c
@@ -0,0 +1,14 @@
+int majorityElement(int* nums, int numsSize) {
+    int count = 0;
+    int majority = 0;
+
+    for(int i = 0; i < numsSize; i++){
+        if(count == 0){
+            majority = nums[i];
+        }
+
+        count += (nums[i] == majority) ? 1 : -1;
+    }
+
+    return majority;
+}
